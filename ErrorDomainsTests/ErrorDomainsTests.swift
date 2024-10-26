079838377631df48d144a766d30b14e67585a0d1
Dmitry Poryadin <dporyadin11@gmail.com>
2024-10-26 22:52:18 +0300

    Initial Commit

---
+++ ErrorDomainsTests/ErrorDomainsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorDomainsTests.swift
+//  ErrorDomainsTests
+//
+//  Created by Дмитрий Порядин on 26.10.2024.
+//
+
+import Testing
+@testable import ErrorDomains
+
+struct ErrorDomainsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
